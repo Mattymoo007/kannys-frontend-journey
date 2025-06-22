94b50ae1d2cf1a6c023f9d4c1d2ee2464eb697a8
Kanny <kanny.gusenga@hexagon.com>
2025-06-22 16:36:49 +0200

    feat: add task display components

---
+++ Step 5 - 8/src/components/TaskItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="checkbox"/>
+    <span>{{ task.title }}</span>
+    <button>🗑️</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ Step 5 - 8/src/components/TaskList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot v-for="task in tasks" :task="task" :key="task.id" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskList',
+  props: {
+    tasks: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
